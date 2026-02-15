36001fdc4698d3b574b972ab0c9b62192ddc44ca
sabasisva77777 <maia_or@yahoo.com>
2026-02-15 09:48:49 +0400

    commat

--- level 0/homework/hw.py
+++ level 0/homework/hw.py
@@ -1,1 +1,54 @@
-print("saba")
+from turtle import*
+
+
+#we want to paint house
+#1 step: paint  a square
+width(7)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+
+
+#stop painting a square
+#2 step: paint a door
+
+left(90)
+forward(70)
+left(90)
+forward(100)
+right(90)
+forward(60)
+right(90)
+forward(100)
+width(7)
+
+#stop painting door
+#3 step:pwint a roof
+
+left(90)
+forward(70)
+left(90)
+forward(200)
+left(45)
+forward(130)
+left(85)
+forward(142)
+width(7)
+
+#stop painting roof
+#4 step:paint windows
+
+left(50)
+forward(50)
+left(90)
+dont paint
+forward(70)
+width(7)
+
+exitonclick()
+
+

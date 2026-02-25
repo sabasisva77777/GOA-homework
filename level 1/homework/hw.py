b2020f045cfd4a302744cc6fd7fab36c92d07d00
sabasisva77777 <maia_or@yahoo.com>
2026-02-25 23:36:53 +0400

    commit

---
+++ level 1/homework/hw.py
@@ -0,0 +1,43 @@
+# 3
+print("saba sisvadze")
+print("nikoloz kvelashvili")
+print("vano motiashvili")
+
+
+# 5
+
+# პითონი არ კითხულობს კომენტარს და კომენტარი არის იმ ადამიანებისთვის რომლებიც კითხულობენ ჩვენს კიდს.
+
+# 6
+
+# ფაილის გაფართოვება არის როდესაც ჩვენ ბრაუზერს ვეუბნებით თუ რომელ ენაში ვმუშაობთ.
+# და პითონის ფაილი იქმნება წერტილი .py გამოყენებით
+
+# 7
+
+# ბეჭდავს იმას რასაც ჩვენ გადავცემთ.
+
+# 8
+# ტერმინალი გამშვები გარემოა სადაც გამოდის ჩვენი კოდის შედეგი.
+
+# 9
+print("saba")
+print("sisvadze")
+print(12)
+
+
+# 10 
+
+print("bad boys")
+print("ketilebi")
+print("usashveloebi")
+print("chemi colis daqalebi")
+print("iuruli parki")
+
+# 11
+
+print("saba","saba","saba","saba","saba","saba","saba","saba","saba","saba","saba","saba")
+
+# 12
+
+print(17)

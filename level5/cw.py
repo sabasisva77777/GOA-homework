1007511b2ef820b9a00188470a3b3e144963bc62
sabasisva77777 <maia_or@yahoo.com>
2026-03-08 15:03:27 +0400

    commit

---
+++ level5/cw.py
@@ -0,0 +1,74 @@
+# 1) ახსენით რა არის debugging
+
+# 2)შექმენით ცვლადი snake case ით
+
+# 3) რამდენი ცვლადი შეიქმნა ამ კოდით:
+# name = "giorgi"
+# Name = "lasha"
+# name = "avto"
+
+# 4)რას გამოიტანს ეს კოდი:
+# age = 10
+# age = 20
+# AGE = 40
+# print(Age)
+
+# 5)შეასწორე ეს კოდები:
+# name = "Lika"
+# print(Name)
+
+# first_user = "Luka"
+# 2nduser = "Giorgi"
+# print(first user)
+
+# name1 = levani
+# 2name = "demetre"
+# print(name1 + 2name)
+
+
+
+# 1 დავალება
+# Debugging არის შეცდომების პოვნა და გასწორება
+
+
+# 2 დავალება
+
+first_name = "საბა"
+
+
+#  3 დავალება
+
+
+name = "giorgi"
+Name = "lasha"
+name = "avto"
+
+# აქ არის ორი ცვლადი: name და Name
+
+
+# დავალება 4
+
+age = 10
+age = 20
+AGE = 40
+print(Age)   # შეცდომაა, ბრჭყალები უნდა.
+
+
+# დავალება 5
+
+# 1
+name = "Luka"
+print(name)
+
+# 2
+first_user = "Luka"
+second_user = "Giorgi"
+print(first_user)
+
+# 3
+
+name1 = "levani"
+name2 = "demetre"
+print(name1 + name2)   # შედეგი: levanidemetre
+
+

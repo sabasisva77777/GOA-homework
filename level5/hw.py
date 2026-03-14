e71769763c14c7f0c66a0c7f58a2fc8f1a07acce
sabasisva77777 <maia_or@yahoo.com>
2026-03-14 11:56:55 +0400

    commit

---
+++ level5/hw.py
@@ -0,0 +1,59 @@
+# 1) ახსენით რა არის snake case
+
+# 2)გაასწორე ეს კოდი
+# name = "Luka"
+# age = 14
+
+# print("Name:", name
+# print("Age:", age)
+
+# 3)რომელ ხაზზე გაჩერდება ეს კოდი
+# animal = "Cat"
+# Animal = "Dog"
+
+# print(animal)
+# print(Animal)
+# print(animal)
+# print(ANIMAL)
+
+# 4)გადააკეთე ცვლადის სახელი:
+# useraccount = "random123"
+
+
+# 5)რას გამოიტანს ეს კოდი
+# name = "Ana"
+# Name = "Luka"
+
+# print(name)
+# print(Name)
+
+
+
+# დავალება 1
+
+# Snake case არის სტილი, სადაც სიტყვები ერთმანეთთან ქვედა ტირით _ არის დაკავშირებული(გამოყოფილი) ის დაშორების ფუნცციას ასრულებს. 
+
+
+# დავალება 2
+
+name = "Luka"
+age = 14
+
+print("Name:", name)
+print("Age:", age)
+
+# დავალება 3
+
+# ეს კოდი გაჩერდება მესამე ხაზზე რადგან ცვლადი ANIMAL არაა მოცემული.
+
+
+# დავალება 4
+
+user_account = "random123"
+# snake case უნდა დავწეროთ
+
+
+
+# დავალება 5
+
+# ეს კოდი გამოიტანს Ana და Luka

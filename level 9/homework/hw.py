a203c7653fe91c19a4446d3fc4bace7f19ebcbf8
sabasisva77777 <maia_or@yahoo.com>
2026-04-12 11:27:54 +0400

    commit

---
+++ level 9/homework/hw.py
@@ -0,0 +1,37 @@
+# 1)მომხმარებლისგან მიიღე რიცხვი string ტიპში და გადაიყვანე int-ად.
+# დაბეჭდე მისი ორმაგი მნიშვნელობა.
+
+# 2)მომხმარებლისგან მიიღე ორი რიცხვი string ტიპში.
+# გადააქციე float ტიპად და დაბეჭდე მათი ჯამი.
+
+# 3)მომხმარებლისგან მიიღე მონაცემი და შეამოწმე არის თუ არა int
+
+# 4)შექმენი ცვლადი და დაბეჭდე მისი ტიპი type() ფუნქციით
+
+# 5)მომხმარებლისგან მიიღე ასაკი string ტიპში
+# გადააქციე int-ად და დაუმატე 5 წელი
+
+
+# davaleba 1
+age=str(input( "your age" ))
+print(int(age*2))
+
+# davaleba 2
+
+first=str(input("Seiyvane ricxvi1" ))
+second=str(input("Seiyvane ricxvi2" ))
+print(float(first+second))
+
+# davaleba 3
+
+# ?
+
+# davaleba 4
+x = 42
+print(type(x))
+
+# davaleba 5
+
+წელი=str(input("weli"))
+წელი=int("weli")
+print(წელი+5)
